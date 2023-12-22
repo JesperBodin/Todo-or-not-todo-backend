c5651bf90af3492c8c0fb4ae3bb2edcdfdd0a9c2
JesperBodin <112554269+JesperBodin@users.noreply.github.com>
2023-12-22 15:09:46 +0100

    change back todoRepository to JPA

--- src/main/java/todo/ornot/todobackend/todo/controller/TodoController.java
+++ src/main/java/todo/ornot/todobackend/todo/controller/TodoController.java
@@ -3,12 +3,14 @@ package todo.ornot.todobackend.todo.controller;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 import todo.ornot.todobackend.todo.dto.TodoDTO;
+import todo.ornot.todobackend.todo.exception.NotFoundException;
 import todo.ornot.todobackend.todo.service.TodoService;
 import todo.ornot.todobackend.todo.entity.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -34,8 +36,9 @@ public class TodoController {
 
     @GetMapping("/{id}")
     public TodoDTO getEntityById(@PathVariable Long id) {
-        Todo todo = todoService.findById(id);
-//                .orElseThrow(() -> new NotFoundException("Todo with id " + id + " not found"));
+        Optional<Todo> todoDto = todoService.findById(id);
+        Todo todo = todoDto.orElseThrow(() ->
+                new NotFoundException("Todo with id " + id + " not found"));
         return todo.todoDTO();
     }
 
--- src/main/java/todo/ornot/todobackend/todo/service/TodoService.java
+++ src/main/java/todo/ornot/todobackend/todo/service/TodoService.java
@@ -2,96 +2,96 @@ package todo.ornot.todobackend.todo.service;
 
 import todo.ornot.todobackend.todo.entity.Todo;
 import todo.ornot.todobackend.todo.exception.NotFoundException;
-import todo.ornot.todobackend.todo.repository.CustomTodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import todo.ornot.todobackend.todo.repository.TodoRepository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class TodoService {
 
-//    private final TodoRepository todoRepository;
+    private final TodoRepository todoRepository;
 
-    private final CustomTodoRepository customTodoRepository;
+//    private final CustomTodoRepository customTodoRepository;
 
 
     @Autowired
-    public TodoService(CustomTodoRepository customTodoRepository) {
-        this.customTodoRepository = customTodoRepository;
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
-//    public List<Todo> findAll() {
-//        return todoRepository.findAll();
+    public List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
+
+//    public List<Todo> findAll(){
+//        return customTodoRepository.findAll();
 //    }
 
-    public List<Todo> findAll(){
-        return customTodoRepository.findAll();
+    public Optional<Todo> findById(Long id){
+        return todoRepository.findById(id);
     }
 
-//    public Optional<Todo> findById(Long id){
-//        return todoRepository.findById(id);
+//    public Todo findById(Long id){
+//        return customTodoRepository.findById(id);
 //    }
 
-    public Todo findById(Long id){
-        return customTodoRepository.findById(id);
+
+    public Todo save(String newTodo, LocalDate dueDate, boolean done) {
+            Todo todo = new Todo(newTodo, dueDate, done);
+            return todoRepository.save(todo);
     }
 
-
-//    public Todo save(String newTodo, LocalDate dueDate, boolean done) {
-//            Todo todo = new Todo(newTodo, dueDate, done);
-//            return todoRepository.save(todo);
+//    public Todo save(String newTodo, LocalDate dueDate, boolean done){
+//        return customTodoRepository.save(newTodo, dueDate, done);
 //    }
 
-    public Todo save(String newTodo, LocalDate dueDate, boolean done){
-        return customTodoRepository.save(newTodo, dueDate, done);
+    public void deleteById(Long id) {
+        todoRepository.findById(id)
+                .ifPresentOrElse(
+                        todo -> todoRepository.deleteById(id),
+                        () -> {
+                            throw new NotFoundException("Failed to delete, no todo with id " + id + " found");
+                        }
+                );
     }
 
-//    public void deleteById(Long id) {
-//        todoRepository.findById(id)
-//                .ifPresentOrElse(
-//                        todo -> todoRepository.deleteById(id),
-//                        () -> {
-//                            throw new NotFoundException("Failed to delete, no todo with id " + id + " found");
-//                        }
-//                );
+//    public void deleteById(Long id){
+//        customTodoRepository.deleteById(id);
 //    }
 
-    public void deleteById(Long id){
-        customTodoRepository.deleteById(id);
+    public void deleteAll() {
+        if(todoRepository.count() > 0) {
+            todoRepository.deleteAll();
+        } else {
+            throw new NotFoundException("You have no todos to delete!");
+        }
     }
 
-//    public void deleteAll() {
-//        if(todoRepository.count() > 0) {
-//            todoRepository.deleteAll();
-//        } else {
-//            throw new NotFoundException("You have no todos to delete!");
-//        }
+//    public void deleteAll(){
+//        customTodoRepository.deleteAll();
 //    }
 
-    public void deleteAll(){
-        customTodoRepository.deleteAll();
+    public Todo updateTodo(Long id, String newTodo, LocalDate dueDate, Boolean done) {
+        Optional<Todo> existingTodoOptional = todoRepository.findById(id);
+
+        if (existingTodoOptional.isPresent()) {
+            Todo existingTodo = existingTodoOptional.get();
+
+            existingTodo.setNewTodo(newTodo);
+            existingTodo.setDueDate(dueDate);
+            existingTodo.setDone(done);
+
+            return todoRepository.save(existingTodo);
+        } else {
+            throw new NotFoundException("Todo with id " + id + " not found");
+        }
     }
 
-//    public Todo updateTodo(Long id, String newTodo, LocalDate dueDate, Boolean done) {
-//        Optional<Todo> existingTodoOptional = todoRepository.findById(id);
-//
-//        if (existingTodoOptional.isPresent()) {
-//            Todo existingTodo = existingTodoOptional.get();
-//
-//            existingTodo.setNewTodo(newTodo);
-//            existingTodo.setDueDate(dueDate);
-//            existingTodo.setDone(done);
-//
-//            return todoRepository.save(existingTodo);
-//        } else {
-//            throw new NotFoundException("Todo with id " + id + " not found");
-//        }
+//    public Todo updateTodo(Long id, String newTodo, LocalDate dueDate, boolean done){
+//        return customTodoRepository.updateTodo(id, newTodo, dueDate, done);
 //    }
-
-    public Todo updateTodo(Long id, String newTodo, LocalDate dueDate, boolean done){
-        return customTodoRepository.updateTodo(id, newTodo, dueDate, done);
-    }
 }
